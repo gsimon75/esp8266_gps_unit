dfd2c56a9d64b9e885cfd189b3e81e6e368e1691
Gabor Simon <gabor.simon75@gmail.com>
2020-12-08 23:53:36 +0400

    gpio button debouncer works

--- main/main.c
+++ main/main.c
@@ -102,43 +102,75 @@ wifi_stop(void) {
 
 
 static xQueueHandle gpio_evt_queue = NULL;
+static TimerHandle_t gpio_debounce_timer[GPIO_NUM_MAX] = { NULL };
 
 static void
-gpio_isr_handler(void *arg)
-{
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+gpio_isr_handler(void *arg) {
+    if (arg) {
+        xTimerResetFromISR((TimerHandle_t)arg, 0);
+    }
 }
+
+
+static void
+gpio_debounce_expired(TimerHandle_t xTimer) {
+    uint8_t gpio_num = (uintptr_t)pvTimerGetTimerID(xTimer);
+    if (gpio_num < GPIO_NUM_MAX) {
+        if (gpio_get_level(gpio_num)) {
+            gpio_num |= 0x80;
+        }
+        xQueueSend(gpio_evt_queue, &gpio_num, 0);
+    }
+}
+
 
 static void
 gpio_task_example(void *arg)
 {
-    uint32_t io_num;
-
     while (true) {
+        uint8_t io_num;
         if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            ESP_LOGI(TAG, "GPIO[%d]=%d", io_num & 0x7f, io_num & 0x80);
         }
     }
 }
 
 
 static void
+gpio_debouncer_add(int gpio_num, int debounce_ms) {
+    if (gpio_debounce_timer[gpio_num]) {
+        xTimerChangePeriod(gpio_debounce_timer[gpio_num], pdMS_TO_TICKS(debounce_ms), 0);
+    }
+    else {
+        gpio_debounce_timer[gpio_num] = xTimerCreate("gpio", pdMS_TO_TICKS(debounce_ms), pdFALSE, (void*)gpio_num, gpio_debounce_expired);
+        gpio_isr_handler_add(gpio_num, gpio_isr_handler, gpio_debounce_timer[gpio_num]);
+    }
+}
+
+
+static void
+gpio_debouncer_remove(int gpio_num) {
+     gpio_isr_handler_remove(gpio_num);
+     xTimerDelete(gpio_debounce_timer[gpio_num], 0);
+     gpio_debounce_timer[gpio_num] = NULL;
+}
+
+
+static void
 button_init() {
-    gpio_config_t io_conf;
-
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    io_conf.pin_bit_mask = 1 << GPIO_BUTTON;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
-
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
-
+    gpio_evt_queue = xQueueCreate(GPIO_NUM_MAX, sizeof(uint8_t));
+    xTaskCreate(gpio_task_example, "gpio", 2048, NULL, 10, NULL);
     gpio_install_isr_service(0);
-    gpio_isr_handler_add(GPIO_BUTTON, gpio_isr_handler, (void *) GPIO_BUTTON);
-    //gpio_isr_handler_remove(GPIO_BUTTON);
+
+    gpio_config_t io_conf = {
+        .pin_bit_mask = 1 << GPIO_BUTTON,
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = 0,
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_ANYEDGE,
+    };
+    gpio_config(&io_conf);
+    gpio_debouncer_add(GPIO_BUTTON, 300);
 }
 
 
