368653bdcc3eb10da61eff4f8e9c0218ebb5b4b4
Gabor Simon <gabor.simon75@gmail.com>
2021-01-27 08:23:15 +0000

    local admin ui: script tag was missing

--- local_admin_ui/join_page.py
+++ local_admin_ui/join_page.py
@@ -20,7 +20,6 @@ script_include_unquoted = re.compile(b"^<script\\s.*\\bsrc=([^\"][^ >]*)")
 with open(path.join(base, "index.html"), "rb") as f, mmap.mmap(f.fileno(), 0, mmap.MAP_PRIVATE, mmap.PROT_READ) as mm:
     start = 0   # processed this far
     end = mm.size()
-    skip_next_script_close = False
 
     while start < end:
         # try to find the start of the next tag
@@ -44,17 +43,15 @@ with open(path.join(base, "index.html"), "rb") as f, mmap.mmap(f.fileno(), 0, mm
 
         if preload_tag.match(tag):
             pass    # skip it
-        elif tag == b"</script>" and skip_next_script_close:
-            skip_next_script_close = False # ... and skip it
         else:
             # check if it's a script include
             match = script_include_quoted.match(tag) or script_include_unquoted.match(tag)
             if match:
                 scriptname = path.join(base, match.group(1).decode())
-                skip_next_script_close = True
 
                 # resolve the include: map it and dump its content
                 with open(scriptname, "rb") as s, mmap.mmap(s.fileno(), 0, mmap.MAP_PRIVATE, mmap.PROT_READ) as script:
+                    sys.stdout.buffer.write(b"<script>")
                     sys.stdout.buffer.write(script)
 
             else:
